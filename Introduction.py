24d3c24c5dccb4d67ca3d400de8ade5f544679f4
davidwall72 <94458977+davidwall72@users.noreply.github.com>
2023-02-02 11:52:10 +0700

    Add files via upload

---
+++ Introduction.py
@@ -0,0 +1,34 @@
+# Libraries
+import streamlit as st
+from PIL import Image
+
+# Layout
+st.set_page_config(page_title='NEAR - The Path to Decentralization', page_icon=':bar_chart:', layout='wide')
+st.title('NEAR - The Path to Decentralization')
+
+# Content
+
+
+# st.image(Image.open('images/near-logo.png'))
+
+st.write(
+    """
+    NEAR is a decentralized blockchain platform that aims to bring blockchain technology to everyday users. 
+    It prioritizes scalability and user experience, while still maintaining a high degree of decentralization. 
+    To achieve this, NEAR uses a unique consensus mechanism called "Nightshade" that allows for faster and more efficient 
+    block validation while still maintaining a distributed network of validators. 
+    Additionally, NEAR has implemented various governance mechanisms, such as token-weighted voting, 
+    to give token holders a direct say in the direction of the network. 
+    These efforts towards decentralization allow for a more democratic and open blockchain ecosystem, 
+    making NEAR a promising platform for decentralized applications and digital assets.
+    """
+)
+
+# st.subheader('Methodology')
+# st.write(
+#     """
+#     To be included soon
+#     """
+# )
+
+
